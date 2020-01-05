0abb496d05bbb5114ea5d563f80a159a2feffbb5
sozemego <piknypiec@gmail.com>
2020-01-05 16:54:16 +0100

    SelectedStar now shows mass of the Star

--- src/game/ui/SelectedStar.tsx
+++ src/game/ui/SelectedStar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useRealClock } from '../util/useRealClock';
 import { Star } from '../object/Star';
-import { SolarSystem } from '../object/SolarSystem';
 
 export function SelectedStar({ star }: SelectedStarProps) {
   useRealClock({ interval: 1000 });
 
   const position = `x: ${star.position.x.toFixed(0)}, y: ${star.position.y.toFixed(0)}`;
   const acceleration = `x: ${star.acceleration.x.toFixed(2)}, y: ${star.acceleration.y.toFixed(2)}`;
+  const mass = star.mass;
   const solarSystem = star.solarSystem;
   const planetCount = solarSystem?.planets.length || 0;
 
@@ -17,6 +17,7 @@ export function SelectedStar({ star }: SelectedStarProps) {
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <img alt={'star'} src={star.texture} style={{ width: 64, height: 64 }} />
       </div>
+      <div>Mass: {mass.toFixed(0)}</div>
       <div>
         <div>Position</div>
         <div>{position}</div>
