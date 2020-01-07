9ad636718f569240000235ff9fcd0339ec13b741
sozemego <piknypiec@gmail.com>
2020-01-07 01:50:45 +0100

    CollisionService improvement - now does not check the same pairs of objects twice

--- src/game/service/CollisionService.ts
+++ src/game/service/CollisionService.ts
@@ -14,25 +14,29 @@ export class CollisionService implements IGameService {
   update(delta: number) {
     let { centerStar, solarSystems } = this.universe;
     const stars = [centerStar!, ...solarSystems.map(system => system.star)];
-    for (let star1 of stars) {
-      for (let star2 of stars) {
-        if (star1 === star2) continue;
+    for (let i = 0; i < stars.length; i++) {
+      let star1 = stars[i];
+      for (let j = i + 1; j < stars.length; j++) {
+        let star2 = stars[j];
         if (star1.sphere.intersectsSphere(star2.sphere)) {
           const starToDestroy = star1.mass > star2.mass ? star2 : star1;
           starToDestroy.solarSystem?.dispose();
         }
       }
     }
+
     for (let solarSystem of solarSystems) {
       const { star } = solarSystem;
       const { planets } = solarSystem;
-      for (let planet1 of planets) {
+
+      for (let i = 0; i < planets.length; i++) {
+        let planet1 = planets[i];
         if (planet1.sphere.intersectsSphere(star.sphere)) {
           planet1.dispose();
           continue;
         }
-        for (let planet2 of planets) {
-          if (planet1 === planet2) continue;
+        for (let j = i + 1; j < planets.length; j++) {
+          let planet2 = planets[j];
           if (planet1.sphere.intersectsSphere(planet2.sphere)) {
             const planetToDestroy = planet1.mass > planet2.mass ? planet2 : planet1;
             planetToDestroy.dispose();
